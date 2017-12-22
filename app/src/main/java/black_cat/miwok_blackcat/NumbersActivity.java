5ca8960c392c079af245e61b41a03e9637e72a49
zevara <varezanoorliko@gmail.com>
2017-12-22 17:18:34 +0700

    a

--- app/src/main/java/black_cat/miwok_blackcat/NumbersActivity.java
+++ app/src/main/java/black_cat/miwok_blackcat/NumbersActivity.java
@@ -2,8 +2,8 @@ package black_cat.miwok_blackcat;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -28,21 +28,15 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("ten");
 
         // Find the root view so we can add child views to it
-        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
 
         // Keep looping until we've reached the end of the list (which means keep looping
         // as long as the current index position is less than the length of the list).
         // The index variable keeps track of our current position in the list and
         // increments by 1 each time the code in the loop is executed.
-        for (int index = 0; index < words.size(); index++) {
-            // Create a new TextView
-            TextView wordView = new TextView(this);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, words);
 
-            // Set the text to be word at the current index
-            wordView.setText(words.get(index));
+        ListView listView = (ListView) findViewById(R.id.list);
 
-            // Add this TextView as another child to the root view of this layout
-            rootView.addView(wordView);
-        }
+        listView.setAdapter(itemsAdapter);
     }
 }
